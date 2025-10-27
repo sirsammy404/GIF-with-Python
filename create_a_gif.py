3745287efb960a2994698c1bd6c3e8ec9521ec0a
Thitiyaporn Apivathananukul <thitiyapornap@gmail.com>
2025-10-27 22:52:18 +0700

    .gitignore

--- create_a_gif.py
+++
@@ -1,9 +0,0 @@
-import imageio.v3 as iio
-
-filenames = ['elmo-1.png','elmo-2.png','elmo-3.png','elmo-4.png','elmo-5.png']
-images = []
-
-for filename in filenames:
-    images.append(iio.imread(filename))
-
-iio.imwrite('elmo.gif',images,duration = 200,loop =0)
